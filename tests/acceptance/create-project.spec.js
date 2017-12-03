a8685a2318e9f3fa473bd1a8a08f31b806c2b540
mlynam <mlynam@iteamnm.com>
2017-12-03 16:26:17 -0700

    test: add create project acceptance test

---
+++ tests/acceptance/create-project.spec.js
@@ -0,0 +1,7 @@
+
+describe('create project', () => {
+  it('should create the project structure');
+  it('should install the npm packages');
+  it('should be buildable');
+  it('should be testable');
+});
