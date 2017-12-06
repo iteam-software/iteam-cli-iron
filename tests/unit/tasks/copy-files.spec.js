1a3525e41651f43f5fe7c45ae69dc152918a1084
Matt Lynam <mlynam@iteamnm.com>
2017-12-06 09:06:29 -0700

    test: add copy-files tests

--- tests/unit/tasks/copy-files.spec.js
+++ tests/unit/tasks/copy-files.spec.js
@@ -1,5 +1,42 @@
 
-const copyFiles = require('../../../lib/tasks/copy-files');
+describe('copy files', () => {
+  let log;
+  let mockLog;
 
-it('should copy files');
-it('should log a message about the copied files');
+  beforeEach(() => {
+    log = console.log;
+    mockLog = jest.fn();
+
+    console.log = mockLog;
+
+    jest.mock('fs-extra', () => ({
+      copy: (src, destination) => new Promise((res, rej) => {
+        res();
+      }),
+    }));
+  });
+
+  it('should copy files', () => {
+    const copyFiles = require('../../../lib/tasks/copy-files');
+
+    return copyFiles('test', 'dest')()
+      .then((msg) => {
+        expect(msg).toEqual(expect.stringContaining('test -> dest'));
+      });
+  });
+
+  it('should log a message about the copied files', () => {
+    const copyFiles = require('../../../lib/tasks/copy-files');
+    
+    return copyFiles('test', 'dest')()
+      .then((msg) => {
+        expect(mockLog.mock.calls.length).toBe(1);
+        expect(mockLog.mock.calls[0][0]).toEqual('Copied data to dest.');
+      });
+  });
+
+  afterEach(() => {
+    console.log = log;
+    jest.resetAllMocks();
+  })
+});
