0a982be7228aa3e399b6498e10725ea9fbac62b4
Matt Lynam <mlynam@iteamnm.com>
2017-12-06 08:55:21 -0700

    feat: add clean-dir tests

--- tests/unit/tasks/clean-dir.spec.js
+++ tests/unit/tasks/clean-dir.spec.js
@@ -1,4 +1,22 @@
 
-const cleanDir = require('../../../lib/tasks/clean-dir');
+it('should clean a given dir', () => {
+  jest.mock('rimraf', () => (p, cb) => {
+    expect(p).toEqual(expect.stringContaining('test'));
+    cb();
+  });
+  const cleanDir = require('../../../lib/tasks/clean-dir');  
 
-it('should clean a given dir');
+  return cleanDir('test')()
+    .then(() => expect.anything());
+});
+
+it('should gracefully handle an error', () => {
+  jest.mock('rimraf', () => (p, cb) => {
+    cb(1);
+  });
+
+  const cleanDir = require('../../../lib/tasks/clean-dir');
+
+  return cleanDir('test')()
+    .catch((err) => expect(err).toBe(1));
+});
