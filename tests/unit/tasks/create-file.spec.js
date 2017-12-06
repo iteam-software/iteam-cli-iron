6164e608e35e7a2d1a5b32e80078621ebcdd7f75
Matt Lynam <mlynam@iteamnm.com>
2017-12-06 10:22:11 -0700

    test(create-file): add covering tests

--- tests/unit/tasks/create-file.spec.js
+++ tests/unit/tasks/create-file.spec.js
@@ -1,7 +1,74 @@
 
-const createFile = require('../../../lib/tasks/create-file');
+describe('create file', () => {
+  let mkdirMock;
 
-it('should make the parent dir if it does not exist');
-it('should write regular string data');
-it('should write a javascript object as a JSON string');
-it('should gracefully handle a creation exception');
+  beforeEach(() => {
+    jest.mock('path', () => ({
+      resolve: (a, b) => `${a}/${b}`,
+      join: (...a) => a.join('/'),
+    }));
+
+    jest.mock('fs', () => ({
+      existsSync: (dir) => !dir.includes('no-dir'),
+      writeFile: (p, d, cb) => {
+        if (d === 'error-me') {
+          cb(1);
+        } else {
+          cb();
+        }
+      },
+    }));
+
+    jest.mock('shelljs', () => {
+      const mkdir = jest.fn();
+      mkdirMock = mkdir;
+      return { mkdir };
+    });
+
+    JSON.stringify = jest.fn();
+  });
+
+  afterEach(() => jest.resetAllMocks());
+
+  it('should make the parent dir if it does not exist', () => {
+    expect.assertions(1);
+
+    const createFile = require('../../../lib/tasks/create-file');
+
+    return createFile('test.file', '', { relativeDir: 'no-dir' })()
+      .then(() => expect(mkdirMock.mock.calls.length).toBe(1));
+  });
+
+  it('should write regular string data', () => {
+    expect.assertions(2);
+
+    const createFile = require('../../../lib/tasks/create-file');
+
+    return createFile('create.file', '')()
+      .then((msg) => {
+        expect(msg).toEqual(expect.stringContaining('[SUCCESS]'));
+        expect(JSON.stringify.mock.calls.length).toBe(0);
+      });
+  });
+
+  it('should write a javascript object as a JSON string', () => {
+    expect.assertions(2);
+
+    const createFile = require('../../../lib/tasks/create-file');
+
+    return createFile('create.file', {})()
+      .then((msg) => {
+        expect(msg).toEqual(expect.stringContaining('[SUCCESS]'));
+        expect(JSON.stringify.mock.calls.length).toBe(1);
+      });
+  });
+
+  it('should gracefully handle a creation exception', () => {
+    expect.assertions(1);
+
+    const createFile = require('../../../lib/tasks/create-file');
+
+    return createFile('create.file', 'error-me')()
+      .catch((msg) => expect(msg).toEqual(expect.stringContaining('[FAILED]')));
+  });
+});
